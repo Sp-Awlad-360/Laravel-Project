e1b1cfa7c8639d9e52f2cba803c8c425807e3a0d
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-21 16:18:12 +0600

    Output.js

--- Javascript/Output.js
+++ Javascript/Output.js
@@ -1,1 +1,22 @@
-output of js
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h1 id="header"></h1>
+
+    <script>
+       <!-- alert("Hello"); -->
+       document.write("Name  : Awlad Hossain<br>");
+       document.write("Mobile: 01518985104<br>");
+       document.write("Email : sp.awladhossain@gmail.com");
+
+       console.log("Name : Awlad Hossain");
+       console.log("Mobile : 01518985104");
+       console.log("Email : sp.awladhossain@gmail.com");
+
+    </script>
+  </body>
+</html>
