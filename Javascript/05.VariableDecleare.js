c5927e82ab0adc2a1e1466ce3238c362cefc4a90
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-21 20:21:10 +0600

    05.VariableDecleare.js

--- Javascript/05.VariableDecleare.js
+++ Javascript/05.VariableDecleare.js
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <p>
+       Variable Rules:
+       1. Variable names can only certain letters, numbers, underscore of dollar      
+          signs. A-Z, a-z, 0-9, _ , $ .
+       2.Variable names must begin with a letter, an underscore of dollar signs.
+       3.Variable names cannot certain spaces.
+       4.Reserved word (like javascript keyword) cannot be used as names.
+       5.Variable names are case-sensitive.
+    </p>
+
+    <script>
+       var first_name, second_name; 
+       first_name='Awlad Hossain';
+       document.write(first_name);
+       
+       second_name='Najmul Islam';
+       document.write(second_name);
+
+    </script>
+  </body>
+</html>
+
 
