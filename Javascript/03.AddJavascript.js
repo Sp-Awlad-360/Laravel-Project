1e9a56c9afcc0836431a378fed17ab363e7680e2
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-21 16:39:19 +0600

    03.AddJavascript.js

--- Javascript/03.AddJavascript.js
+++ Javascript/03.AddJavascript.js
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body onload="alert('I am from Inline javascript')">
 
+   
+    <script>
+       alert("I am from internal javascript");
+    </script>
+
+
+    <script src="/src/script.js"></script>
+  </body>
+</html>
+
+
+-----  External Javascript  -----
+
+alert("I am from external javascript");
