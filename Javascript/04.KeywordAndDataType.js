5805caff33830a0be74bece74de37491e9b2bc39
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-21 17:01:34 +0600

    04.KeywordAndDataType.js

--- Javascript/04.KeywordAndDataType.js
+++ Javascript/04.KeywordAndDataType.js
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <!-- Keyword -->
+    <!-- break, case, catch, continue, debugger, default, delete, do, else, finally, for, function, -->
+    <!-- if, in, instanceof, new, return, switch, this, throw, try, typeof, var, void, while, with. -->
 
+    <!-- Data type -->
+    01.String
+    02.Number
+    03.Bolean
+
+  </body>
+</html>
