1cde7716cf005ecf337d9e8f5aa645397d883507
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-21 21:26:56 +0600

    07.Concatenation.js

--- Javascript/07.Concatenation.js
+++ Javascript/07.Concatenation.js
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+  
+    <script>
+       document.write("<h1>Concatenation</h1><br><br>");
+       document.write("Awlad"+" Hossain<br>");
+       
+       var first_name, last_name;
+       first_name = "Awlad";
+       last_name = " Hossain";
+       full_name = first_name + last_name;
+       document.write("My Name is "+ full_name +" And my age 25 years old.");
 
+    </script>
+  </body>
+</html>
