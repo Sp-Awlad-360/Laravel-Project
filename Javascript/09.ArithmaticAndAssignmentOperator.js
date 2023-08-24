d271a0cfeda4ba41cd3b726b6e36abe6f126d478
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-25 01:33:52 +0600

    09.ArithmaticAndAssignmentOperator.js

--- Javascript/09.ArithmaticAndAssignmentOperator.js
+++ Javascript/09.ArithmaticAndAssignmentOperator.js
@@ -1,1 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+  
+    <script>
+       document.write("<h1>Arithmatic Operator</h1><br>");
+       var x,y;
+       x=5, y=2;
+       x+=y;
+       document.write("Addition = "+x+"<br>");
+       x=5, y=2;
+       x-=y;
+       document.write("Substract = "+x+"<br>");
+       x=5, y=2;
+       x*=y;
+       document.write("Multiplication = "+x+"<br>");
+       x=5, y=2;
+       x/=y;
+       document.write("Divisioin = "+x+"<br>");
+       x=5, y=2;
+       x%=y;
+       document.write("Modulus = "+x+"<br>");
+       
+       document.write("<h1>Assignment Operator</h1><br>");
+       var x,y;
+       x=5, y=2;
+       x+=y;
+       document.write("Addition = "+x+"<br>");
+       x=5, y=2;
+       x-=y;
+       document.write("Substract = "+x+"<br>");
+       x=5, y=2;
+       x*=y;
+       document.write("Multiplication = "+x+"<br>");
+       x=5, y=2;
+       x/=y;
+       document.write("Divisioin = "+x+"<br>");
+       x=5, y=2;
+       x%=y;
+       document.write("Modulus = "+x+"<br>");
+       
+      
 
+    </script>
+  </body>
+</html>
