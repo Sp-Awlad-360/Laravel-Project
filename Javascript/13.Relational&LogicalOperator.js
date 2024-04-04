b382ee7f9b1bc696a7dacf35b477edbcc95f413b
Awlad Hossain <140268506+sp-awlad@users.noreply.github.com>
2024-04-04 23:53:31 +0600

    13.Relational&LogicalOperator.js

---
+++ Javascript/13.Relational&LogicalOperator.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Relational & Logical</title>
+</head>
+<body>
+    
+    <script>
+        // Relational Operator-> >, >=, <, <=, ==, ===, !=, !==
+        document.write(20>10);    //True
+        document.write(20>=10);   //True
+        document.write(20<10);    //False
+        document.write(20<=10);   //False
+        document.write(20==10);   //False -> Just value check
+        document.write(20==="20");//False-> value and data type check
+        document.write(20!=10);   //True -> Just value check
+        document.write(20!==10);  //True-> value and data type check
+
+         // Logical Operator-> &&, ||, !
+        document.write(20>10 && 10>15);  //False
+        document.write(20>10 || 10>15);  //True
+        document.write(!(20>10));        //False
+
+    </script>
+</body>
+</html>
+
+
+
+
+
+
+
+
