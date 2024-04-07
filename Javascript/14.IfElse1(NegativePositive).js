6946ad48bb1d324f6f5df0ff04014a3bf448723b
Awlad Hossain <140268506+sp-awlad@users.noreply.github.com>
2024-04-07 22:42:17 +0600

    14.IfElse1(NegativePositive).js

---
+++ Javascript/14.IfElse1(NegativePositive).js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>If, Else if, Else</title>
+  </head>
+  <body>
+    <div>
+      <h2>If, Else if, Else</h2>
+    </div>
+    
+    <script type="text/javascript" charset="utf-8">
+      var num1 = prompt("Eneter a number : ");
+      if(num1>0)
+      {
+        document.write("Positive Number");
+      }
+      else if(num1<0)
+      {
+        document.write("Negative Number");
+      }
+      else
+      {
+        document.write("This is Zero");
+      }
+    </script>
+  </body>
+</html>
